7dd9bfbc8904e09acdf22b0485c5367fd4cf474d
MU <m.iouriev@gmail.com>
2024-02-22 21:41:41 +0700

    22.02.2024

--- SEMINAR/Task9.java
+++ SEMINAR/Task9.java
@@ -2,7 +2,6 @@ package SEMINAR;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Arrays;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
